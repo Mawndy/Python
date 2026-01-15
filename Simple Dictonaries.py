6e186d1be23f4b74e865ffd2d6ea9ab13178d1c7
Mawndy <bulletgun234@gmail.com>
2026-01-15 18:43:34 +0800

    Adding comment for better explanation

--- Simple Dictonaries.py
+++ Simple Dictonaries.py
@@ -13,5 +13,6 @@ print (worker["name"])
 worker["age"] = 31  
 worker["occupation"] = "Senior Software Developer"
 
+# Printing all key-value pairs
 for key, value in worker.items():
     print(key, value)
